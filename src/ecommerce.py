f27771e8f55721afdef7223a42cf292e689e2563
Marcus Backstrom <VBMarcusVB@hotmail.com>
2025-10-28 15:04:56 +0100

    La till rev_per_category i metrics.py och ecommernce.py och använden den funktionen i Report.ipynb för att visa top 3

--- src/ecommerce.py
+++ src/ecommerce.py
@@ -1,6 +1,6 @@
 import pandas as pd
 from .io_utiles import load_file
-from .metrics import (rev_unit_count)
+from .metrics import (rev_unit_count, rev_per_category)
 
 class EcommerceAnalyzer:
     def __init__(self, csv_path: str):
@@ -10,5 +10,5 @@ class EcommerceAnalyzer:
         return rev_unit_count(self.df)
     
     def rev_per_category(self):
-        return NotImplemented
+        return rev_per_category(self.df)
     
