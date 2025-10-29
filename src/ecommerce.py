2092ceef9e8ac158d15d1d0b83f40843d1e9efb8
Alex <ahlarn@gmail.com>
2025-10-29 14:46:20 +0100

    skapade en funktion i viz.py för att visualisera försäljning över tid, la till klassen i ecommerce.py samt la till diagrammet i report.ipynb

--- src/ecommerce.py
+++ src/ecommerce.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from .io_utiles import load_file
 from .metrics import (rev_unit_count)
+from .viz import (revenue_per_month)
 
 class EcommerceAnalyzer:
     def __init__(self, csv_path: str):
@@ -9,3 +10,5 @@ class EcommerceAnalyzer:
     def rev_unit_count(self):
         return rev_unit_count(self.df)
     
+    def rev_per_month_viz(self):
+        return revenue_per_month(self.df)
